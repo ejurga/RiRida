4832b7c2e1f104f35d678c08ea7dc54be05f28ea
ejurga <ejacc@protonmail.com>
2024-09-13 13:37:29 -0400

    moves func to file

---
+++ R/get_samples.R
@@ -0,0 +1,31 @@
+#' Get sample information from all samples in an irida project
+#'
+#' @param project_id The IRIDA project ID
+#'
+#' @return A dataframe with all the samples in a project, along with some
+#'         of the metadata fields I expect to always be filled.
+#'
+#' @export
+project_samples <- function(project_id){
+
+  samples <-
+    req_irida() %>%
+    req_url_path_append("projects") %>%
+    req_url_path_append(project_id) %>%
+    req_url_path_append("samples") %>%
+    req_perform() %>%
+    resp_body_json()
+
+  x <- samples$resource$resources
+
+  df <-
+    tibble(
+              id = sapply(x, function(x) x$identifier),
+     sample_name = sapply(x, function(x) x$sampleName),
+     createdDate = as_datetime(sapply(x, function(x) x$createdDate/1000)),
+    modifiedDate = as_datetime(sapply(x, function(x) x$modifiedDate/1000)),
+     description = sapply(x, function(x) x$description)
+    )
+
+  return(df)
+}
